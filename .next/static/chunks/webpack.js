da4d83e6c0e0c974292826adf5bc40083a452614
JIdayyy <julien_abbadie@hotmail.fr>
2021-08-21 21:59:12 +0200

    admin layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9e0d009830637fa99126";
+/******/ 	var hotCurrentHash = "30af7d8e43be2adcf9f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
