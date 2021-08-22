fb0e08747c1d7abc6c77a44fa4413df7294b5c2e
JIdayyy <julien_abbadie@hotmail.fr>
2021-08-22 20:53:41 +0200

    reset

--- src/components/admin/Views/User.tsx
+++ src/components/admin/Views/User.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 export default function User() {
-    const { data, isLoading, errors } = useQuery("getUsers", () =>
-        axios.get(process.env.NEXT_PUBLIC_API_URL),
+    const { data, isLoading, error } = useQuery("getUsers", () =>
+        console.log(""),
     );
     return <div></div>;
 }
