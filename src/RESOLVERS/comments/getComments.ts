75ef8d1a2f9ff03a5897dbc22fd2facc7e3f22d7
JIdayyy <julien_abbadie@hotmail.fr>
2021-09-15 08:34:49 +0200

    prisma client version

--- src/RESOLVERS/comments/getComments.ts
+++ src/RESOLVERS/comments/getComments.ts
@@ -1,23 +1,30 @@
 import { Comment, Prisma } from ".prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../../../prisma/client";
+import prisma from "../../../prisma/client";
 
 export default async function getComments(
     req: NextApiRequest,
     res: NextApiResponse<
-        Comment[] | { name: string; message: string; code: string; test?: any }
+        | Comment[]
+        | {
+              name: string;
+              message: string;
+              code: string;
+              test?: any;
+              another?: any;
+          }
     >,
 ): Promise<void> {
     try {
         const comments = await prisma.comment.findMany();
         res.status(200).json(comments);
     } catch (error: unknown) {
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error instanceof Prisma.PrismaClientValidationError) {
             console.log(error);
             return res.status(500).json({
                 name: "Error",
                 message: error.message,
-                code: error.code,
+                code: error.stack,
                 test: error,
             });
         } else {
