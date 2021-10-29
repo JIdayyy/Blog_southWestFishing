898f1049242704169ca7598e3cf934c5009e369f
JIdayyy <julien_abbadie@hotmail.fr>
2021-10-29 22:09:44 +0200

    jwt auth ok

---
+++ pages/api/auth/register.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../src/RESOLVERS/auth/auth";
+
+interface Data {
+    email: string;
+    password: string;
+}
+
+const registerHandler = (
+    req: NextApiRequest,
+    res: NextApiResponse<Data | Data[] | Error | null>,
+): Promise<void> | void => {
+    if (req.method === "POST") {
+        return auth.register(req, res);
+    }
+    throw new Error("Method not allowed");
+};
+export default registerHandler;
--- src/RESOLVERS/auth/auth.ts
+++ src/RESOLVERS/auth/auth.ts
@@ -1,9 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import register from "./methods/register";
 import login from "./methods/login";
 
-const comment = {
+const auth = {
     login: async (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
         login(req, res),
+    register: async (
+        req: NextApiRequest,
+        res: NextApiResponse,
+    ): Promise<void> => register(req, res),
 };
 
-export default comment;
+export default auth;
